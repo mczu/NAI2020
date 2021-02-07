f32d797a8a85a6019d2ed60db43fa40a6b74c668
mczu <14128860+mczu@users.noreply.github.com>
2021-02-07 23:21:06 +0100

    add model and agent

--- 7-RL/Lunar-Lander/Lander.py
+++ 7-RL/Lunar-Lander/Lander.py
@@ -1,7 +1,61 @@
+# 1. Testing environment with OpenAI Gym
 import gym
+
 env = gym.make('LunarLander-v2')
-env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
-env.close()
+states = env.observation_space.shape[0]
+
+for i_episode in range(5):
+    observation = env.reset()
+    score = 0
+    for t in range(100):
+        env.render()
+        # print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        score += reward
+        if done:
+            print("Episode finished after {} timesteps".format(t + 1))
+            break
+    print('Episode:{} Score:{}'.format(i_episode, score))
+
+nb_actions = env.action_space.n  # number of actions in Lunar Lander
+
+# 2.  Creating Model with Keras
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.optimizers import Adam
+
+
+def build_model(states, actions):
+    model = Sequential()
+    model.add(Flatten(input_shape=(1, states)))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(actions, activation='linear'))
+    return model
+
+
+model = build_model(states, nb_actions)
+model.summary()
+
+# 3. Building Agent with Keras-RL
+
+from rl.agents import DQNAgent, SARSAAgent
+from rl.policy import BoltzmannQPolicy, EpsGreedyQPolicy
+from rl.memory import SequentialMemory
+
+# policy = EpsGreedyQPolicy()
+policy = BoltzmannQPolicy()
+memory = SequentialMemory(limit=10000, window_length=1)
+agent = SARSAAgent(model=model, nb_actions=nb_actions, nb_steps_warmup=10, policy=policy)
+agent.compile(Adam(lr=1e-4), metrics=['mae'])
+
+# 4. Teaching the agent
+steps = 5000
+
+agent.fit(env, nb_steps=steps, visualize=False)
+
+# 5. Testing
+
+agent.test(env, nb_episodes=10, visualize=True)
